fe9fd8dcdd0c7bf263a2ad03b2bf1d0834b60fce
ShankarAdd <shankarnrraibagi@gmail.com>
2024-12-04 14:02:37 +0530

    Updated for working CICD

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -5,5 +5,5 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/cucumber",glue = "shankarnrraibagi.stepDefinition",monochrome = true,tags = "@ErrorValidation",plugin = {"html:target/cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
-
+//just added comment to see CICD
 }
